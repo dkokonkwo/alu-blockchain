683e622e15f8313381c51fd836bd6774fe4ffa16
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-15 17:14:53 -0400

    new func

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -7,11 +7,17 @@
  */
 void blockchain_destroy(blockchain_t *blockchain)
 {
-if (blockchain)
-{
-if (llist_destroy(blockchain->chain, 1, block_destroy) != 0)
+block_t *current *temp;
+if (!blockchain)
 return;
+block = llist_pop(blockchain->chain);
+while (block)
+{
+temp = block;
+block_destroy(temp);
+block = llist_pop(blockchain->chain);
+}
+llist_destroy(blockchain->chain, 0, NULL)
 free(blockchain);
 return;
-}
 }
