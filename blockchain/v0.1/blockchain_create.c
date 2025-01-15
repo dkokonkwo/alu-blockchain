40411977153f63f3f98360a80c2194ac45b4f747
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-15 13:52:01 -0400

    new file

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,44 +1,63 @@
 #include "blockchain.h"
 
-#define HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+#define GENESIS_TIMESTAMP 1537578000
+#define GENESIS_DATA "Holberton School"
+#define GENESIS_DATA_LEN 16
+
 /**
  * blockchain_create - creates a blockchain structure and initializes it
  * Return: pointer to blockchain else NULL
  */
 blockchain_t *blockchain_create(void)
 {
-    llist_t *llist;
     blockchain_t *nblockchain;
-    block_t *nblock = (block_t *)malloc(sizeof(block_t));
+    block_t *nblock;
+    llist_t *llist;
+
+    nblock = (block_t *)malloc(sizeof(block_t));
     if (!nblock)
+    {
+        fprintf(stderr, "Failed to allocate memory for genesis block\n");
         return (NULL);
+    }
+
     nblock->info.index = 0;
     nblock->info.difficulty = 0;
     nblock->info.nonce = 0;
-    nblock->info.timestamp = 1537578000;
+    nblock->info.timestamp = GENESIS_TIMESTAMP;
     memset(nblock->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-    memcpy(nblock->data.buffer, "Holberton School", 16);
-    nblock->data.len = 16;
-    memcpy(nblock->hash, HASH, SHA256_DIGEST_LENGTH);
+    memcpy(nblock->data.buffer, GENESIS_DATA, GENESIS_DATA_LEN);
+    nblock->data.len = GENESIS_DATA_LEN;
+    memcpy(nblock->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
     llist = llist_create(MT_SUPPORT_TRUE);
     if (!llist)
     {
+        fprintf(stderr, "Failed to create blockchain list\n");
         free(nblock);
         return (NULL);
     }
+
     if (llist_add_node(llist, nblock, ADD_NODE_FRONT) != 0)
     {
+        fprintf(stderr, "Failed to add genesis block to blockchain list\n");
         llist_destroy(llist, 1, NULL);
         free(nblock);
         return (NULL);
     }
+
+    // Allocate memory for the blockchain structure
     nblockchain = (blockchain_t *)malloc(sizeof(blockchain_t));
     if (!nblockchain)
     {
+        fprintf(stderr, "Failed to allocate memory for blockchain structure\n");
         llist_destroy(llist, 1, NULL);
         free(nblock);
         return (NULL);
     }
+
     nblockchain->chain = llist;
     return (nblockchain);
 }
+
