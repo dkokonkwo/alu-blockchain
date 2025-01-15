5a5625722eaa463e87984aead74ae0dd71c880d2
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-15 13:22:02 -0400

    blockchain

---
+++ blockchain/v0.1/README.md
@@ -0,0 +1,9 @@
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+How a Blockchain is structured
+How Blocks refer to each other
+How immutability is maintained in a Blockchain
+What is the Genesis Block, and why is it important
+How to hash a Block, and what information is stored in the hash
---
+++ blockchain/v0.1/blockchain_create.c
@@ -0,0 +1,44 @@
+#include "blockchain.h"
+
+#define HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+/**
+ * blockchain_create - creates a blockchain structure and initializes it
+ * Return: pointer to blockchain else NULL
+ */
+blockchain_t *blockchain_create(void)
+{
+    llist_t *llist;
+    blockchain_t *nblockchain = NULL;
+    block_t *nblock = (block_t *)malloc(sizeof(block_t));
+    if (!nblock)
+        return (NULL);
+    nblock->info.index = 0;
+    nblock->info.difficulty = 0;
+    nblock->info.nonce = 0;
+    nblock->info.timestamp = 1537578000;
+    nblock->info.prev_hash = {0};
+    nblock->data.buffer = "Holberton School";
+    nblock->data.len = 16;
+    memcpy(nblock->hash, HASH, SHA256_DIGEST_LENGTH);
+    llist = llist_create(MT_SUPPORT_TRUE);
+    if (!llist)
+    {
+        free(nblock);
+        return (NULL);
+    }
+    if (llist_add_node(llist, nblock, ADD_NODE_FRONT) != 0)
+    {
+        llist_destroy(llist);
+        free(nblock);
+        return (NULL);
+    }
+    nblockchain = (blockchain_t *)malloc(sizeof(blockchain_t));
+    if (!nblockchain)
+    {
+        llist_destroy(llist);
+        free(nblock);
+        return (NULL);
+    }
+    nblockchain->chain = llist;
+    return (nblockchain);
+}
