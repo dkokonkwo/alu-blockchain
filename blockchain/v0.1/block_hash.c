2a42b04c7272960972baf9b77e87588e5eba4640
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-16 12:21:09 -0400

    reduced length of line

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -6,7 +6,8 @@
  * @hash_buf: buffer to store the hash
  * Return: pointer to @hash_buf
  */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block,
+uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 size_t hash_len = sizeof(block->info) + block->data.len;
 return (sha256((int8_t const *)block, hash_len, hash_buf));
