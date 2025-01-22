9705f5f75bce0e682dbf37e20ee7f882187dbdfa
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-22 17:02:00 -0400

    calloc instead of malloc

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -11,12 +11,11 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 tx_in_t *input;
 if (!unspent)
 return (NULL);
-input = (tx_in_t *)malloc(sizeof(tx_in_t));
+input = calloc(1, sizeof(input));
 if (!input)
 return (NULL);
 memcpy(input->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
 memcpy(input->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
 memcpy(input->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
-input->sig.len = NULL;
 return (input);
 }
