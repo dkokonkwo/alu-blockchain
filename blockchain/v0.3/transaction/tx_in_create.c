1abf2e97fb1871b77d65f2f175532779d5ae5711
dkokonkwo <d.okonkwo@alustudent.com>
2025-01-22 16:53:04 -0400

    tx_in_create.c

---
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -0,0 +1,21 @@
+#include "./blockchain.h"
+#include "transaction.h"
+
+/**
+ * tx_in_create - initializes transaction input structure
+ * @unspent: pointer to unspent transaction output
+ * Return: pointer to created transaction input structure, or NULL on failure
+ */
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
+{
+tx_in_t *input;
+if (!unspent)
+return (NULL);
+input = (tx_in_t *)malloc(sizeof(tx_in_t));
+if (!input)
+return (NULL);
+memcpy(input->block_hash, unspent->block_hash, SHA256_DIGEST_LENGTH);
+memcpy(input->tx_id, unspent->tx_id, SHA256_DIGEST_LENGTH);
+memcpy(input->tx_out_hash, unspent->out.hash, SHA256_DIGEST_LENGTH);
+return (input);
+}
